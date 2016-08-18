dbf368bb7c7ad91ee59e1c183b1d2b4ed59fb638
櫻本静香 <shizuka.sakuramoto.0916@gmail.com>
2016-08-18 22:21:48 +0900

    Can set current_user at application_base_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,6 @@
 class ApplicationController < ActionController::Base
 #   before_filter :require_login
   protect_from_forgery with: :null_session
-  # before_action :set_current_user
 
-  private
-
-  def set_current_user
-    auth_token = ApiKey.find_by(access_token: request.headers[:token])
-    @current_user = auth_token.user
-  end
-
-#   def not_authenticated
-#     redirect_to login_path, alert: "Please login first"
-#   end
 
 end
