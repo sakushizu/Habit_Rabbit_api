9601c45152ed833fcf8fccdd0cc9e4267a2f7838
櫻本静香 <shizuka.sakuramoto.0916@gmail.com>
2016-08-28 15:37:35 +0900

    Change migration file

--- db/migrate/20160506142247_create_user_dates.rb
+++ db/migrate/20160506142247_create_user_dates.rb
@@ -2,7 +2,7 @@ class CreateUserDates < ActiveRecord::Migration
   def change
     create_table :user_dates do |t|
       t.references :user, index: true, foreign_key: true
-      t.datetime :tapped_date
+      t.string :name
       t.integer :calendar_id
       t.integer :user_id
 
--- db/migrate/20160609052724_create_calendars.rb
+++ db/migrate/20160609052724_create_calendars.rb
@@ -1,12 +1,11 @@
 class CreateCalendars < ActiveRecord::Migration
   def change
     create_table :calendars do |t|
-      t.string :title null: false
+      t.string :title
       t.integer :color_R
       t.integer :color_G
       t.integer :color_B
       t.string :orner
-
       t.timestamps null: false
     end
   end
