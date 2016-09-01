dbac84f4bc71a374763515614495ffcb333e7bda
櫻本静香 <shizuka.sakuramoto.0916@gmail.com>
2016-09-01 12:00:08 +0900

    add comment out

--- app/controllers/api/user_date_controller.rb
+++ app/controllers/api/user_date_controller.rb
@@ -1,57 +1,57 @@
-class Api::UserDateController < ApplicationController
-  before_action :set_note, only: [:show, :edit, :update, :destroy]
+# class Api::UserDateController < ApplicationController
+#   before_action :set_note, only: [:show, :edit, :update, :destroy]
 
-  def index
-    @dates = UserDate.accessible_by(current_ability)
-  end
+#   def index
+#     @dates = UserDate.accessible_by(current_ability)
+#   end
 
-  def show
-  end
+#   def show
+#   end
 
-  def new
-    @date = UserDate.new
-  end
+#   def new
+#     @date = UserDate.new
+#   end
 
-  def edit
-  end
+#   def edit
+#   end
 
-  def create
-    @date = UserDate.new(date_params.merge(user: current_user))
+#   def create
+#     @date = UserDate.new(date_params.merge(user: current_user))
 
-    respond_to do |format|
-      if @date.save
-        format.json { render :show, status: :created, location: @date }
-      else
-        format.json { render json: @date.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+#     respond_to do |format|
+#       if @date.save
+#         format.json { render :show, status: :created, location: @date }
+#       else
+#         format.json { render json: @date.errors, status: :unprocessable_entity }
+#       end
+#     end
+#   end
 
-  def update
-    respond_to do |format|
-      if @date.update(date_params)
-        format.json { render :show, status: :ok, location: @date }
-      else
-        format.json { render json: @date.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+#   def update
+#     respond_to do |format|
+#       if @date.update(date_params)
+#         format.json { render :show, status: :ok, location: @date }
+#       else
+#         format.json { render json: @date.errors, status: :unprocessable_entity }
+#       end
+#     end
+#   end
 
-  def destroy
-    @date.destroy
-    respond_to do |format|
-      format.json { head :no_content }
-    end
-  end
+#   def destroy
+#     @date.destroy
+#     respond_to do |format|
+#       format.json { head :no_content }
+#     end
+#   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_note
-      @date = UserDate.find(params[:id])
-    end
+#   private
+#     # Use callbacks to share common setup or constraints between actions.
+#     def set_note
+#       @date = UserDate.find(params[:id])
+#     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def date_params
-      params.require(:user_date).permit(:user_id, :calendar_id, :tapped_date)
-    end
-end
+#     # Never trust parameters from the scary internet, only allow the white list through.
+#     def date_params
+#       params.require(:user_date).permit(:user_id, :calendar_id, :tapped_date)
+#     end
+# end
